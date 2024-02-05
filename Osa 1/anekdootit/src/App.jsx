b667e7a71799cd09e6385ed540fc3c2849593cb0
Taija Mitronen <taiski.92@gmail.com>
2024-02-05 11:40:00 +0200

    ylimääräiset kommentit pois

--- Osa 1/anekdootit/src/App.jsx
+++ Osa 1/anekdootit/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 const VoteStatistics = (props) => {
-  console.log(props, ' Vote Statisticsin propsit')
     return (
       <p>has {props.votelist[props.selected]} votes</p>
     )
