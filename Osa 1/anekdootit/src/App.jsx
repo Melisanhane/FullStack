444d898fec384b07e4f5e3da1a7b79a5cdd0c969
Taija Mitronen <taiski.92@gmail.com>
2024-02-05 10:25:53 +0200

    Osa 1-anekdootit-App.JSX päivitys

--- Osa 1/anekdootit/src/App.jsx
+++ Osa 1/anekdootit/src/App.jsx
@@ -1,22 +1,19 @@
 // import { useState } from 'react'
 
-// uLKOISIIN EI SAADA VIETYÄ JUURIKOMPONENTISTA
-// Vote statistics || TOIMII
+// Vote statistics || TOIMII siten, että Debuggaamalla tekee sen mitä haluan mutta ei tulosta
 const VoteStatistics = (props) => {
-  // Alkuarvot toimii
   console.log(props, ' Vote Statisticsin propsit')
-  /* TOIMII
-  if (props.votelist[props.vote] === 0) {
+  debugger
+  if (props.votelist === 0) {
     return ( 
       <p>No votes</p>
     )
   }
-  */
-
-  return (
-    <p>has {props.votelist[props.selected]} votes </p>
-  )
-  
+  if (props > 0) {
+    return (
+      <p>has {props} votes </p>
+    )
+  }
 }
 
 // Voittaja || TOIMII KOVAKOODATTUNA
@@ -29,16 +26,14 @@ const Statistics = ({votelist, anecdotes}) => {
       location = i
     }
   }
-  console.log('Eniten ääniä: ')
-  console.log(anecdotes[location])
+//  console.log('Eniten ääniä: ')
+//  console.log(anecdotes[location])
   return (
     <p>{anecdotes[location]}
     < br/>
     has {biggest} votes</p>
   )
 }
- 
-
 
 const Button = ({handleClick, text}) => (
   <button onClick={handleClick}> {text} </button>
@@ -59,8 +54,16 @@ const App = () => {
   const votes = new Array(anecdotes.length).fill(0)  // Uusi lista
   let selected = 0  // Määritellään alku
 
+   // Vote || TOIMII mutta ei saa näytille / Vienti onnistuu vain return VoteStatistics
+   const Vote = () => { 
+    votes[selected] += 1
+    console.log(votes, 'votesin lista')
+    return VoteStatistics (
+        votes[selected]
+    )
+  }
 
-// Random || TOIMII mutta ei saa näytille / vietiä staticsiin
+// Random || TOIMII mutta ei saa näytille / vietyä
   const RandomAnecdote = () => {
     selected = Math.floor(Math.random()*8)
     console.log(anecdotes[selected])
@@ -71,20 +74,12 @@ const App = () => {
       </>
     )
   }
- // Vote || TOIMII mutta ei saa näytille / vietiä staticsiin
-  const Vote = () => { 
-    votes[selected] += 1
-    console.log(votes, 'votesin lista')
-    return (
-        <VoteStatistics votelist={votes} />
-    )
-  }
 
   return (
     <div>
       <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
-      <VoteStatistics votelist={votes}/>
+      <VoteStatistics votelist={votes[selected]}/>
       <br/>
       <Button handleClick={Vote} text='vote' />
       <Button handleClick={RandomAnecdote} text='next anecdote' />
@@ -93,6 +88,7 @@ const App = () => {
     </div>
   )
 }
+
 /*
 <Statistics anecdotes={anecdotes} votelist={votes} selected={selected} />
       <Statistics votelist={votes} vote={selected} />
@@ -102,18 +98,3 @@ const App = () => {
 
 
 export default App
-/*
-return (
-    
-  <div>
-    <h1>Anecdote of the day</h1>
-    <Random value={selected} anecdotes={anecdotes}/>
-    <VoteTally currentVote={voteCopy[selected]}/>
-    <Button handleClick={() => {voteCopy[selected] += 1; setVote(voteCopy)}} name="vote"/>
-    <Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} name="next anecdote"/>
-    <h1>Anecdote with the most votes</h1>
-    <VoteLeader anecdotes={anecdotes} vote={vote}/>
-  </div>
-)
-}
-*/
