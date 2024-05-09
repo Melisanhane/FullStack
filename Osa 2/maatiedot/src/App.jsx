92a4d78bb0e629dbbc6d366d06907220eb53b9eb
Taija Mitronen <taiski.92@gmail.com>
2024-05-09 12:41:44 +0300

    Maatieto tehtävän palautus

--- Osa 2/maatiedot/src/App.jsx
+++ Osa 2/maatiedot/src/App.jsx
@@ -1,53 +1,51 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-/*
-Tämä käynnistää sovelluksen ja tästä valitaan millä ohjelmalla käynnistät
-export VITE_SOME_KEY=54l41n3n4v41m34rv0 && npm run dev // 
-  Linux/macOS Bash <--. Tällä koska BASH
-($env:VITE_SOME_KEY="54l41n3n4v41m34rv0") -and (npm run dev) // 
-  Windows PowerShell
-set "VITE_SOME_KEY=54l41n3n4v41m34rv0" && npm run dev // 
-  Windows cmd.exe
-Tämä VITE-SOME-KEY lisätään jotenkin näin
-axios
-        .get(`https:/api.openweathermap.org/data/2.5/weather?q=${countriesToShow[0].capital[0]}&appid=${process.env.VITE_SOME_KEY}`)
-        */
 
-const CountryWeather = (country) => {
-//  const apiKey = import.meta.env.REACT_APP_VITE_KEY
-  const [capitalWeather, setCapitalWeather] = useState(null)
-  const apiKey = import.meta.env.REACT_APP_VITE_KEY
-  console.log(apiKey)
- 
-  useEffect(() => { async () => { 
-    try {
-      const response = await 
-        axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${apiKey}&units=metric`)
-        setCapitalWeather(response.data)
-    }
-    catch(error){console.error('Error fetching weather data:', error)} 
-  }}, [apiKey]) 
-  }
-// Tämän pitää näyttää maa painamalla show nappia eli yksittäinen maatiedot tähän 
-// ei palauta mtn
 const ShowCountry = (country) => {
   console.log(country)
-  CountryWeather(country)
+  const apiKey = import.meta.env.VITE_OPEN_WEATHER_API
+  const [capitalWeather, setCapitalWeather] = useState(null)
+
+  console.log(apiKey)
+  useEffect(() => {
+    getWeather(country.latlng[0], country.latlng[1]);
+  });
+
+  const getWeather = async (lat, lon) => {
+    await axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${apiKey}`)
+      .then((response) => setCapitalWeather(response.data));
+  };
   return (
     <div>
-    <h1>{country.name.common}</h1>
-    <p>capital: {country.capital[0]}</p>
-    <p>area: {country.area}</p>
-    <h3>languages</h3>
-    <ul>
-      {Object.entries(country.languages).map(([code, lang]) => (
-        <li key={code}>{lang}</li>
-      ))}
-    </ul>
-    <img src={country.flags.png} alt={country.flags.alt}></img>
-    <CountryWeather />
-  </div>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital[0]}</p>
+      <p>Area: {country.area}</p>
+      <div>
+        <h2>Languages</h2>
+        <ul>
+          {Object.values(country.languages).map((language) => (
+            <li key={language}>{language}</li>
+          ))}
+        </ul>
+        <div>
+          <img src={country.flags["png"]} />
+        </div>
+        {capitalWeather && (
+          <div>
+            <h2>Weather in {country.capital[0]}</h2>
+            <p>Temperature: {capitalWeather.main.temp} Celcius</p>
+            <div>
+              <img
+                src={`https://openweathermap.org/img/wn/${capitalWeather.weather[0].icon}@2x.png`}
+                alt={capitalWeather.weather[0].description}
+              />
+              <p>Wind: {capitalWeather.wind.speed} mph</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
 )
 };
 
@@ -57,8 +55,8 @@ const App = () => {
   const [allCountries, setAllCountries] = useState('')
   const [countries, setCountries] = useState([])
 
-// Haetaan aluksi kaikki maat palvelimelta 
   useEffect(() => {
+    console.log('Haetaan maat sivustolta ensimmäisenä')
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/all/`)
       .then((result) => {
@@ -69,65 +67,31 @@ const App = () => {
       })
   }, [])
 
-// Haetaan maat palvelimelta SHOW btn tapahtuma 
-const countrySelected = (country) => {
-  axios
-    .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country.name.common}`)
-    .then((response) => {
-      setCountries(response.data)
-      console.log('GET succesfully')
-      ShowCountry(response.data)
-    })
-    .catch((error) => {
-      console.log('GET error message')
-    })
-  }
-
 const CountriesToShow = () => { 
-// console.log(".")
-//  Maiden filtteröinti 
-let filterCountries = allCountries.filter((country) =>
-  country.name.common.toLowerCase().startsWith(search.toLowerCase())
-)
-if (search !== '') {
-  if (filterCountries.length > 10) {
-    return (
-      <div>Too many matches, specify another filter</div>
-    )
+  let filterCountries = allCountries.filter((country) =>
+    country.name.common.toLowerCase().startsWith(search.toLowerCase())
+  )
+  if (search !== '') {
+    if (filterCountries.length > 10) {
+      return (
+        <div>Too many matches, specify another filter</div>
+      )
+    }
+    if (filterCountries.length === 1) {
+      const country = filterCountries[0]
+      return ShowCountry(country)
+    }
+    else {
+      return filterCountries.map((country) => (
+        <div key={country.cca3}>
+            <span>
+              {country.name.common}
+              <button onClick={() => ShowCountry(country)}>show</button>
+            </span>
+          </div>
+      ))
+    }
   }
-  else if (filterCountries.length === 1) {
-    const country = filterCountries[0]
-//    CountryWeather(country)
-    ShowCountry(country)
-  /*
-    return (
-      <div>
-        <h1>{country.name.common}</h1>
-        <p>capital: {country.capital[0]}</p>
-        <p>area: {country.area}</p>
-        <h3>languages</h3>
-        <ul>
-          {Object.entries(country.languages).map(([code, lang]) => (
-            <li key={code}>{lang}</li>
-          ))}
-        </ul>
-        <img src={country.flags.png} alt={country.flags.alt}></img>
-        <CountryWeather />
-      </div>
-    )
-  */  
-  }
-  else {
-    return filterCountries.map((country) => (
-      <div key={country.cca3}>
-          <span>
-            {country.name.common}
-            <button onClick={() => countrySelected(country)}>show</button>
-          </span>
-        </div>
-    ))
-  }
-} // Tähän astri pois
 }
 
   const handleSearchChange = (event) => {
