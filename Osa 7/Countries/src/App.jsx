39531e6378db64299918cf4e30466a1daf21c963
Taija Mitronen <taiski.92@gmail.com>
2024-10-14 20:23:08 +0300

    7.7

--- Osa 7/Countries/src/App.jsx
+++ Osa 7/Countries/src/App.jsx
@@ -16,32 +16,64 @@ const useField = (type) => {
 }
 
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+  const [country, setCountry] = useState('')
+  const [allCountries, setAllCountries] = useState([])
 
-  useEffect(() => {})
+  // Haetaan kaikki maat ensiksi palvelimelle
+  useEffect(() => {
+    axios
+    .get(`https://studies.cs.helsinki.fi/restcountries/api/all/`)
+    .then((result) => {
+      setAllCountries(result.data)
+    })
+    .catch((error) => {
+      console.log('error message')
+    })
+  }, [])
 
-  return country
+  // Etsitään yksittäinen maa palvelimelta
+  useEffect(() => {
+    if (name) {
+      axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then(res => {
+        setCountry(allCountries.filter((country) =>
+          country.name.common.toLowerCase().startsWith(name)
+        ))
+      })
+      .catch(error => {
+        console.log('error')
+        setCountry('error')
+      })
+    }
+  }, [name])
+
+  if (name === '' || !country) {
+    return
+  }
+  if (country === 'error') {
+    return country
+  }
+  return country[0]
+
 }
 
 const Country = ({ country }) => {
   if (!country) {
     return null
   }
-
-  if (!country.found) {
+  if (country === "error") {
     return (
       <div>
         not found...
       </div>
     )
   }
-
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{country.name.common} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div> 
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/>  
     </div>
   )
 }
@@ -60,9 +92,8 @@ const App = () => {
     <div>
       <form onSubmit={fetch}>
         <input {...nameInput} />
-        <button>find</button>
+        <button type="submit">find</button>
       </form>
-
       <Country country={country} />
     </div>
   )
