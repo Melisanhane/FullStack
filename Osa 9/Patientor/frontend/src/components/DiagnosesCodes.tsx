6fa494510dea79b54eb010ddf7be5d59267fa532
Taija Mitronen <taiski.92@gmail.com>
2025-01-13 14:21:50 +0200

    Diagnoosikoodien päivitys

--- Osa 9/Patientor/frontend/src/components/DiagnosesCodes.tsx
+++ Osa 9/Patientor/frontend/src/components/DiagnosesCodes.tsx
@@ -19,16 +19,21 @@ const MenuProps = {
 };
 
 const diagnosesCodes = [
+  'M24.2',
+  'M51.2',
+  'S03.5',
+  'J10.1',
+  'J06.9',
   'Z57.1',
   'N30.0',
+  'H54.7',
+  'J03.0',
   'L60.1',
-  'R78.1',
-  'M67.0',
-  'Z20.1',
-  'Y90.1',
-  'T03.0',
-  'N99.1',
-  'P45.1',
+  'Z74.3',
+  'L20',
+  'F43.2',
+  'S62.5',
+  'H35.29'
 ];
 
 export default function MultipleSelectCheckmarks() {
